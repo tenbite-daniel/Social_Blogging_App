d3da1c0c3ddbce0cbc442d60b0307b705a7d8fb1
Tenbite Daniel <101597746+tenbite-daniel@users.noreply.github.com>
2025-08-03 22:52:16 +0300

    Update corsOptions.js
    added deployed frontend url corsOptions

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,4 +1,4 @@
-const whitelist = ["https://www.frontend.com", "http://localhost:5173", "http://localhost:5174"];
+const whitelist = ["https://social-blogging-app-mauve.vercel.app", "http://localhost:5173", "http://localhost:5174"];
 
 export const corsOptions = {
     origin: (origin, callback) => {
